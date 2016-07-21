f408573947856992457a72435b8f265c9071b71a
IvanDobrovolsky <dobrovolsky94@gmail.com>
2016-07-22 00:06:36 +0300

    Fully implemented HomePageComponent.

--- src/app/components/pages/home/home-page.component.ts
+++ src/app/components/pages/home/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -7,8 +8,15 @@ import { Component, ViewEncapsulation } from '@angular/core';
     templateUrl: 'home-page.component.html',
     styleUrls: [
         'home-page.component.css'
+    ],
+    directives: [
+        ROUTER_DIRECTIVES
     ]
 })
+
 export class HomePageComponent {
-    
+    private applicationTitle = "angular2-online-store";
+    private description    = "This is simple online store of computers implemented with Angular2 to demonstrate how the framework shines and makes the way we create apps more logical and straightforward.";
+    private repositoryLink = "https://github.com/IvanDobrovolsky/angular2-online-store";
+    private bannerImageUrl = "https://www.ag-grid.com/images/angular2.png";
 }
