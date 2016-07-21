75dc777ba94fb791f9dbcd8549fe879b7d09bfaa
IvanDobrovolsky <dobrovolsky94@gmail.com>
2016-07-21 23:31:00 +0300

    Added 404 route handler.

---
+++ src/app/components/pages/404/404-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    encapsulation: ViewEncapsulation.Emulated,
+    selector: 'page-404',
+    templateUrl: '404-page.component.html',
+    styleUrls: [
+        '404-page.component.css'
+    ]
+})
+export class PageNotFoundComponent {
+    
+}
--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -2,11 +2,14 @@ import { RouterConfig, provideRouter } from '@angular/router';
 
 //Application components
 import { HomePageComponent }     from './../components/pages/home/home-page.component';
-import { MainPageComponent } from './../components/pages/main/main-page.component';
+import { MainPageComponent }     from './../components/pages/main/main-page.component';
+import { PageNotFoundComponent } from './../components/pages/404/404-page.component';
 
+//Application routes
 const routes: RouterConfig = [
-    { path: '', component: HomePageComponent },
-    { path: 'main',   component: MainPageComponent }
+    { path: '',     component: HomePageComponent },
+    { path: 'main', component: MainPageComponent },
+    { path: '**',   component: PageNotFoundComponent}
 ];
 
 export const appRouterProviders = [
