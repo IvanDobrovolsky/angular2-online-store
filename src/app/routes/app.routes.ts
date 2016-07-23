02b7894b4aaa655cc351cf1e780bdd17e046f588
IvanDobrovolsky <dobrovolsky94@gmail.com>
2016-07-24 00:20:06 +0300

    Added a route for shopping cart page.

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -3,7 +3,8 @@ import { RouterConfig, provideRouter } from '@angular/router';
 //Application components
 import { HomePageComponent }     from './../components/pages/home/home-page.component';
 import { CatalogPageComponent }  from '../components/pages/catalog/catalog-page.component';
-import { CatalogItemPreview}     from './../components/pages/catalog/catalog-item-preview/catalog-item-preview.component';
+import { CatalogItemPreview }    from './../components/pages/catalog/catalog-item-preview/catalog-item-preview.component';
+import { ShoppingCartComponent } from './../components/pages/shopping-cart/shopping-cart-page.component';
 import { PageNotFoundComponent } from './../components/pages/404/404-page.component';
 
 //Application routes
@@ -11,7 +12,8 @@ const routes: RouterConfig = [
     { path: '',            component: HomePageComponent },
     { path: 'catalog',     component: CatalogPageComponent },
     { path: 'catalog/:id', component: CatalogItemPreview },
-    { path: '**',          component: PageNotFoundComponent}
+    { path: 'cart',        component: ShoppingCartComponent },
+    { path: '**',          component: PageNotFoundComponent }
 ];
 
 export const appRouterProviders = [
