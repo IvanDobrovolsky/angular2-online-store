61790f31ad35cdb89e0b289048adb19bb9129f2c
IvanDobrovolsky <dobrovolsky94@gmail.com>
2016-07-29 02:06:07 +0300

    Implemented Model-driven form for AdminCreateComponent.

--- src/app/components/pages/admin/create/admin-create.component.ts
+++ src/app/components/pages/admin/create/admin-create.component.ts
@@ -1,4 +1,18 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+    Component,
+    ViewEncapsulation,
+    OnInit,
+    OnDestroy
+}  from '@angular/core';
+
+import {
+    FormGroup,
+    FormBuilder,
+    REACTIVE_FORM_DIRECTIVES,
+    Validators
+} from '@angular/forms';
+
+import { NgClass } from '@angular/common';
 
 @Component({
     moduleId: module.id,
@@ -8,9 +22,50 @@ import { Component, ViewEncapsulation } from '@angular/core';
     styleUrls: [
         'admin-create.component.css'
     ],
-    directives: [],
+    directives: [
+        REACTIVE_FORM_DIRECTIVES,
+        NgClass
+    ],
     providers: []
 })
 export class AdminCreateComponent{
-    
+
+
+    private computerForm: FormGroup;
+    private isSubmitted = false;
+    pristine;
+
+    constructor(private formBuilder: FormBuilder) {
+
+    }
+
+    //TODO Add some more validation rules
+    ngOnInit(): void {
+        this.computerForm = this.formBuilder.group({
+            title:       ['', Validators.required],
+            brand:       ['', Validators.required],
+            price:       ['', Validators.required],
+            image:       ['', Validators.required],
+            description: ['', Validators.required],
+            details:     ['', Validators.required]
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.computerForm = null;
+    }
+
+    private isValidFormField(field): boolean {
+        return !(this.computerForm.controls[field].valid || (this.computerForm.controls[field].pristine && !this.isSubmitted))
+    }
+
+    private add(): void {
+        this.isSubmitted = true;
+
+        if(this.computerForm.valid) {
+            //TODO make api call here
+        }
+    }
+
+    //TODO implement cancel method
 }
