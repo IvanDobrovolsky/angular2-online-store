42e85a17859276886226bf5aae08f52442ba1f71
IvanDobrovolsky <dobrovolsky94@gmail.com>
2016-08-01 17:07:32 +0300

    Moved all the form logic from AdminCreateComponent to new reusable ComputerFormComponent.

---
+++ src/app/components/common/forms/computer-form.component.ts
@@ -0,0 +1,132 @@
+import {
+    Component,
+    ViewEncapsulation,
+    OnInit,
+    OnDestroy,
+    Input,
+    Output,
+    EventEmitter
+}   from '@angular/core';
+import {
+    FormGroup,
+    FormBuilder,
+    REACTIVE_FORM_DIRECTIVES,
+    Validators
+}   from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { Router }  from '@angular/router';
+
+import { Computer }   from './../../../models/computer.model';
+
+//TODO Allow deactivation only if the form is submitted
+
+//In the form all fields are treated as strings
+interface IInitialFormValues {
+    title: string;
+    brand: string
+    price: string;
+    image: string;
+    details: string;
+    description: string;
+}
+
+@Component({
+    moduleId: module.id,
+    encapsulation: ViewEncapsulation.Emulated,
+    selector: 'computer-form',
+    templateUrl: 'computer-form.component.html',
+    styleUrls: [
+        'computer-form.component.css'
+    ],
+    directives: [
+        REACTIVE_FORM_DIRECTIVES,
+        NgClass
+    ],
+    providers: []
+})
+export class ComputerFormComponent implements OnInit, OnDestroy{
+
+    @Input()  private action: string;
+    @Input()  private itemToUpdate: Computer;
+    @Output() private onSubmit = new EventEmitter<Computer>();
+    
+    private computerForm: FormGroup;
+    private initialFormValues: IInitialFormValues = {
+        title: '',
+        brand: '',
+        price: '',
+        image: '',
+        description: '',
+        details: ''
+    };
+    private isSubmitted = false;
+
+    private static pricePattern = "^([1-9]|[0-9][0-9]|[0-9][0-9][0-9]|[0-9][0-9][0-9][0-9])$";
+    private static imageUrlPattern = "(https?:\/\/.*\.(?:png|jpg|jpeg|gif))";
+    private static CSVPattern = `(?:'[^']*')|(?:[^, ]+)`;
+
+    constructor(private formBuilder: FormBuilder, private router: Router) {
+
+    }
+    
+    ngOnInit(): void {
+ 
+        switch (this.action) {
+            case 'update': //Setting initial values
+                if(!!this.itemToUpdate) {
+                    this.initialFormValues = {
+                        title: this.itemToUpdate.title,
+                        brand: this.itemToUpdate.brand,
+                        price: String(this.itemToUpdate.price),
+                        image: this.itemToUpdate.image,
+                        description: this.itemToUpdate.description,
+                        details: this.itemToUpdate.details.join(',')
+                    };
+                } else {
+                    console.error("No item passed!");
+                }
+                break;
+            case 'create': //The form is empty by default
+            default:
+        }
+
+        //Initializing the form
+        this.computerForm = this.formBuilder.group({
+            title:       [this.initialFormValues.title,       Validators.required],
+            brand:       [this.initialFormValues.brand,       Validators.required],
+            price:       [this.initialFormValues.price,       [Validators.required, Validators.pattern(ComputerFormComponent.pricePattern)]],
+            image:       [this.initialFormValues.image,       [Validators.required, Validators.pattern(ComputerFormComponent.imageUrlPattern)]],
+            description: [this.initialFormValues.description, Validators.required],
+            details:     [this.initialFormValues.details,     [Validators.required, Validators.pattern(ComputerFormComponent.CSVPattern)]]
+        });
+        
+    }
+
+    ngOnDestroy(): void {
+        this.computerForm = null;
+    }
+
+    private isValidFormField(field): boolean {
+        return this.computerForm.controls[field].valid || (this.computerForm.controls[field].pristine && !this.isSubmitted);
+    }
+
+    private submit(): void {
+        this.isSubmitted = true;
+
+        if(this.computerForm.valid) {
+
+            const formData = this.computerForm.value;
+
+            const newComputer: Computer = Object.assign(formData, {details: formData.details.split(','), date: Date.now(), _id: Date.now()});
+
+            //Emitting the event in here
+            this.onSubmit.emit(newComputer)
+        }
+    }
+
+    private cancel() {
+        if (this.computerForm.pristine || confirm("Are you sure that you want to quit? All the data will be lost.")) {
+            this.router.navigate(['/admin']);
+        }
+    }
+}
--- src/app/components/pages/admin/create/admin-create.component.ts
+++ src/app/components/pages/admin/create/admin-create.component.ts
@@ -1,10 +1,11 @@
 import {Component, ViewEncapsulation, OnInit, OnDestroy}              from '@angular/core';
-import {FormGroup, FormBuilder, REACTIVE_FORM_DIRECTIVES, Validators} from '@angular/forms';
-import { NgClass }                                                    from '@angular/common';
 import { Router }                                                     from '@angular/router';
 
 import { Computer }   from './../../../../models/computer.model';
+
 import { ApiService } from './../../../../services/api.service';
+
+import { ComputerFormComponent } from './../../../common/forms/computer-form.component';
 
 //TODO Add Allow deactivation only if the form is submitted
 
@@ -17,69 +18,26 @@ import { ApiService } from './../../../../services/api.service';
         'admin-create.component.css'
     ],
     directives: [
-        REACTIVE_FORM_DIRECTIVES,
-        NgClass
+        ComputerFormComponent
     ],
     providers: []
 })
 export class AdminCreateComponent{
-
-
-    private computerForm: FormGroup;
-    private isSubmitted = false;
-
-    private static pricePattern = "^([1-9]|[0-9][0-9]|[0-9][0-9][0-9]|[0-9][0-9][0-9][0-9])$";
-    private static imageUrlPattern = "(https?:\/\/.*\.(?:png|jpg|jpeg|gif))";
-    private static CSVPattern = `(?:'[^']*')|(?:[^, ]+)`;
     
-    constructor(private formBuilder: FormBuilder, private router: Router, private apiService: ApiService) {
-
+    constructor(private apiService: ApiService, private router: Router) {
+        
     }
-    
-    ngOnInit(): void {
-        this.computerForm = this.formBuilder.group({
-            title:       ['', Validators.required],
-            brand:       ['', Validators.required],
-            price:       ['', [Validators.required, Validators.pattern(AdminCreateComponent.pricePattern)]],
-            image:       ['', [Validators.required, Validators.pattern(AdminCreateComponent.imageUrlPattern)]],
-            description: ['', Validators.required],
-            details:     ['', [Validators.required, Validators.pattern(AdminCreateComponent.CSVPattern)]]
-        });
-    }
+    private createComputer(computer: Computer) {
 
-    ngOnDestroy(): void {
-        this.computerForm = null;
-    }
-
-    private isValidFormField(field): boolean {
-        return this.computerForm.controls[field].valid || (this.computerForm.controls[field].pristine && !this.isSubmitted);
-    }
-
-    private add(): void {
-        this.isSubmitted = true;
-
-        if(this.computerForm.valid) {
-
-            const formData = this.computerForm.value;
-
-            const newComputer: Computer = Object.assign(formData, {details: formData.details.split(','), date: Date.now(), _id: Date.now()});
-
-            this.apiService
-                .createNewComputer(newComputer)
-                .subscribe(response => {
-                        if(response.success){
-                            //TODO add notification from NotificationService
-                            console.log(response.message);
-                            this.router.navigate(['/admin']);
-                        }
-                    },
-                    error => console.error(`An error has occurred! ${error}`));
-        }
-    }
-
-    private cancel() {
-        if (this.computerForm.pristine || confirm("Are you sure that you want to quit? All the data will be lost.")) {
-            this.router.navigate(['/admin']);
-        }
+        this.apiService
+            .createNewComputer(computer)
+            .subscribe(response => {
+                    if(response.success){
+                        //TODO add notification from NotificationService
+                        console.log(response.message);
+                        this.router.navigate(['/admin']);
+                    }
+                },
+                error => console.error(`An error has occurred! ${error}`));
     }
 }
