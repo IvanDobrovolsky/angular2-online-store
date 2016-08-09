abe7d1f325c00486ebe1871e8f20b01a86cb95e6
IvanDobrovolsky <dobrovolsky94@gmail.com>
2016-08-10 02:11:05 +0300

    Started rewriting the ShoppingCartService.

--- src/app/components/pages/shopping-cart/shopping-cart-page.component.ts
+++ src/app/components/pages/shopping-cart/shopping-cart-page.component.ts
@@ -2,17 +2,16 @@ import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 import { Router }                                          from '@angular/router';
 import { HTTP_PROVIDERS }                                  from '@angular/http';
 import { NgFor }                                           from '@angular/common';
+import { Observable }                                      from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 import { Computer } from './../../../models/computer.model';
 
 import { ShoppingCartItemComponent } from './shopping-cart-item/shopping-cart-item.component';
 
 import { ApiService }          from './../../../services/api.service';
-import { ShoppingCartService } from './../../../services/shopping-cart-service';
+import { ShoppingCartService, IShoppingCartItem } from './../../../services/shopping-cart-service';
 
-interface IShoppingCartItem extends Computer {
-    quantity: number;
-}
 
 @Component({
     moduleId: module.id,
@@ -30,7 +29,7 @@ interface IShoppingCartItem extends Computer {
 })
 export class ShoppingCartComponent implements OnInit, OnDestroy{
 
-    private items: Array<IShoppingCartItem> = [];
+    private items: Array<Computer> = [];
     private total = 0;
 
     constructor(private apiService: ApiService, private shoppingCartService: ShoppingCartService) {
@@ -40,19 +39,36 @@ export class ShoppingCartComponent implements OnInit, OnDestroy{
     public ngOnInit(): void {
 
         //RX, subject
-        const cartItems = this.shoppingCartService.getCartItems();
 
-        //TODO Think how to use RxJs methods instead, flatMap -> Reduce
-        cartItems.forEach(item => {
-                this.apiService
-                    .getComputerById(item._id)
-                    .subscribe(response => {
-                        if (response.success) {
-                            const computer: Computer = response.data[0];
-                            this.items.push(Object.assign(computer, {quantity: item.quantity}));
-                        }
-                    }, error => console.error(`An error has occurred! ${error}`));
-            });
+        //noinspection TypeScriptUnresolvedFunction
+        this.shoppingCartService
+            .cartItems
+            //.mergeMapTo(cartItems => {
+            //    console.log(cartItems);
+            //    return this.apiService.getComputerById(cartItems[0]._id)
+            //})
+            .subscribe(response => {
+                console.log(response);
+                //if (response.success) {
+                //    const computer: Computer = response.data[0];
+                //    this.items.push(computer);
+                //}
+            }, error => console.error(`An error has occurred! ${error}`));
+        this.shoppingCartService.load();
+
+
+
+        ////TODO Think how to use RxJs methods instead, flatMap -> Reduce
+        //cartItems.forEach(item => {
+        //        this.apiService
+        //            .getComputerById(item._id)
+        //            .subscribe(response => {
+        //                if (response.success) {
+        //                    const computer: Computer = response.data[0];
+        //                    this.items.push(Object.assign(computer, {quantity: item.quantity}));
+        //                }
+        //            }, error => console.error(`An error has occurred! ${error}`));
+        //    });
 
         //TODO Use Observable function instead
         setTimeout(() => this.total = this.calculateTotal(), 500);
@@ -66,17 +82,17 @@ export class ShoppingCartComponent implements OnInit, OnDestroy{
     }
 
     private calculateTotal(): number {
-        console.log(this.items);
-        let total = 0;
-        this.items.forEach(item => {
-            total += item.price * item.quantity;
-        });
-        return total;
+        //console.log(this.items);
+        //let total = 0;
+        //this.items.forEach(item => {
+        //    total += item.price * item.quantity;
+        //});
+        return 0;
     }
 
     //TODO use observable data service instead
     private changeQuantity(id:number, newQuantity): void {
-        this.shoppingCartService.changeQuantity(id, newQuantity);
+        //this.shoppingCartService.changeQuantity(id, newQuantity);
         this.total = this.calculateTotal();
     }
 
--- src/app/services/shopping-cart-service.ts
+++ src/app/services/shopping-cart-service.ts
@@ -1,67 +1,89 @@
 import { Injectable }              from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { Observable }              from 'rxjs/Observable';
+import { Subject }                 from 'rxjs/Subject';
 
 //TODO add caching
-interface IShoppingCartItem {
+export interface IShoppingCartItem {
     _id: number;        //unique key,
     quantity: number;   //quantity of these computers in the cart
 }
 
 interface IShoppingCartService {
-    getCartItems(): IShoppingCartItem[];
-    getCartSize(): number;
-    addToCart(id: number): void;
-    removeFromCart(id: number): void;
-    changeQuantity(id: number, newQuantity: number): void;
+      cartItems: Observable<IShoppingCartItem[]>;
+//    getCartSize(): number;
+//    addToCart(id: number): void;
+//    removeFromCart(id: number): void;
+//    changeQuantity(id: number, newQuantity: number): void;
+}
+
+interface ICartStore {
+    cart: IShoppingCartItem[];
 }
 
 @Injectable()
 export class ShoppingCartService implements IShoppingCartService {
 
-    private cart: Array<IShoppingCartItem> = [];
+    private cart: Subject<IShoppingCartItem[]> ;
+    private cartStore: ICartStore = {cart: []};
 
     constructor() {
-        if(localStorage.getItem('items')){
-            this.cart = JSON.parse(localStorage.getItem('items'));
-        }
+        //this.cart = new Observable<IShoppingCartItem[]>(observer => {
+        //    if(localStorage.getItem('items')){
+        //        const data = JSON.parse(localStorage.getItem('items'));
+        //        data.forEach(item => observer.next(item));
+        //    }
+        //});
+
+        this.cart = <Subject<IShoppingCartItem[]>> new Subject();
     }
 
-    public getCartItems(): IShoppingCartItem[]{
-        return this.cart;
+    public get cartItems(): Observable<IShoppingCartItem[]>{
+        return this.cart.asObservable();
+    }
+
+    public load() {
+        if(localStorage.getItem('items')){
+            this.cartStore.cart = JSON.parse(localStorage.getItem('items'));
+            this.cart.next(this.cartStore.cart);
+        }
     }
 
     public addToCart(_id: number): void{
-        if(this.cart.find(item => item._id === _id)){
-            console.warn('The computer is already in the cart!');
-        } else {
-            this.cart.unshift({
-                _id,
-                quantity: 1
-            });
-            this.updateCart(this.cart);
-            console.info('Successfully added to the cart!');
-        }
+        //if(this.cart.find(item => item._id === _id)){
+        //    console.warn('The computer is already in the cart!');
+        //} else {
+        //    this.cart.unshift({
+        //        _id,
+        //        quantity: 1
+        //    });
+        //    this.updateCart(this.cart);
+        //    console.info('Successfully added to the cart!');
+        //}
+        console.log(this.cart)
     }
-
-    public getCartSize(): number{
-        return this.cart.length;
-    }
-
-    private updateCart(items: IShoppingCartItem[]): void{
-        localStorage.setItem('items', JSON.stringify(items));
-    }
-
+    //
+    //public getCartSize(): number{
+    //    return this.cart.length;
+    //}
+    //
+    //private updateCart(items: IShoppingCartItem[]): void{
+    //    localStorage.setItem('items', JSON.stringify(items));
+    //}
+    //
     public changeQuantity(_id: number, newQuantity: number): void{
-        let itemIndex: number = this.cart.findIndex(item => item._id === _id);
-        this.cart[itemIndex].quantity = newQuantity;
-        this.updateCart(this.cart);
+        //let itemIndex: number = this.cart.findIndex(item => item._id === _id);
+        //this.cart[itemIndex].quantity = newQuantity;
+        //this.updateCart(this.cart);
     }
-
+    //
     public removeFromCart(_id: number): void{
-        let itemIndex: number = this.cart.findIndex(item => item._id === _id);
-        this.cart.splice(itemIndex, 1);
-        this.updateCart(this.cart);
+        //let itemIndex: number = this.cart.findIndex(item => item._id === _id);
+        //this.cart.splice(itemIndex, 1);
+        //this.updateCart(this.cart);
     }
-
+    //
+    //getTotal() {
+    //
+    //}
 }
