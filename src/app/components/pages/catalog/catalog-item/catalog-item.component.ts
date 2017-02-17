0efe5b71d644d6d9e96b2654671dd384f71f3a9f
IvanDobrovolsky <dobrovolsky94@gmail.com>
2017-02-17 13:23:42 +0200

    Refactored catalog item component

--- src/app/components/pages/catalog/catalog-item/catalog-item.component.ts
+++ src/app/components/pages/catalog/catalog-item/catalog-item.component.ts
@@ -1,65 +1,29 @@
-import { Component,Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { Router }                                                from '@angular/router';
-import { Store } from '@ngrx/store';
+import { Component,Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store }  from '@ngrx/store';
 
 import { Computer } from '../../../../models';
-
-import { ShoppingCartService } from "../../../../services";
 
 import * as fromRoot from '../../../../reducers';
 import * as shoppingCart from '../../../../actions/shopping-cart.action';
 
-//TODO Remove interface and make the properties private
-interface ICatalogItem {
-    brand: string;
-    title: string;
-    price: number;
-    image: string;
-    description: string;
-    date: number;
-    preview(id: number): void;
-    addToCart(id: number): void;
-}
-
 @Component({
-    encapsulation: ViewEncapsulation.Emulated,
     selector: 'catalog-item',
     templateUrl: 'catalog-item.component.html',
     styleUrls: [
         'catalog-item.component.css'
-    ],
-    providers: []
+    ]
 })
-export class CatalogItemComponent implements OnInit, OnDestroy, ICatalogItem{
-    public brand: string;
-    public title: string;
-    public price: number;
-    public image: string;
-    public date:  number;
-    public description: string;
+export class CatalogItemComponent {
 
     @Input() private item: Computer;
 
     constructor(
         private router: Router,
-        private shoppingCartService: ShoppingCartService,
         private store: Store<fromRoot.State>
-    ){
+    ){}
 
-    }
-
-    public ngOnInit(): void {
-        Object.assign(this, this.item);
-    }
-
-    public ngOnDestroy():any {
-        delete this;
-    }
-
-    public addToCart(id: number): void {
-        //Deprecated
-        //this.shoppingCartService.addToCart(id);
-
+    public addToCart(): void {
         this.store.dispatch(
             new shoppingCart.AddToCartAction(Object.assign({product: this.item, quantity: 0}))
         )
